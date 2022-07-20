206f325fffca70b568234655abc341d663471ae0
Luiz Franzon <luizfranzon05@hotmail.com>
2022-07-20 13:48:05 -0300

    BackEnd structure created

---
+++ BackEnd/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send()
+})
+
+app.listen(port)
+
+console.log("------------------=------------------")
+console.log(` Servidor Iniciado na porta: ${port} `)
+console.log("------------------=------------------")
