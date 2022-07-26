8cf8e6831ee960e8183a796d0dd337f7ce5f65b9
Luiz Franzon <luizfranzon05@hotmail.com>
2022-07-26 16:03:18 -0300

    InputPanel layout done

--- App/FrontEnd/src/components/InputPanel/index.tsx
+++ App/FrontEnd/src/components/InputPanel/index.tsx
@@ -1,9 +1,29 @@
+import { FloppyDisk } from "phosphor-react"
+
 import styles from "./InputPanel.module.css"
 
 export function InputPanel() {
     return (
         <div className={styles.panel}>
-            <h1>INPUT PANEL</h1>
+            <div className={styles.dataInputBox}>
+                <div className={styles.inputBox}>
+                    <label htmlFor="">Name:</label>
+                    <input type="text" />
+                </div>
+                <div className={styles.inputBox}>
+                    <label htmlFor="">Address:</label>
+                    <input type="text" />
+                </div>
+                <div className={styles.inputBox}>
+                    <label htmlFor="">Birthdate:</label>
+                    <input type="text" />
+                </div>
+            </div>
+            <div className={styles.buttons}>
+                <button className={styles.saveButton}> 
+                    <FloppyDisk size={32}/>
+                </button>
+            </div>
         </div>
     )
 }
