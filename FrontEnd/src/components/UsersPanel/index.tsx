9ca5d1c476b15d90fc03b4c2637b368b7c6f8213
Luiz Franzon <luizfranzon05@hotmail.com>
2022-07-25 23:25:20 -0300

    Created UserPanel component

---
+++ FrontEnd/src/components/UsersPanel/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./UserPanel.module.css"
+
+export function UsersPanel() {
+    return (
+        <div className={styles.panel}>
+            <h1>USERS PANEL</h1>
+        </div>
+    )
+}
