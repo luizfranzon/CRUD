586f5884a07f506667e7ad2a5822258247a737f8
Luiz Franzon <luizfranzon05@hotmail.com>
2022-07-25 23:25:32 -0300

    Created InputPanel component

---
+++ FrontEnd/src/components/InputPanel/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./InputPanel.module.css"
+
+export function InputPanel() {
+    return (
+        <div className={styles.panel}>
+            <h1>INPUT PANEL</h1>
+        </div>
+    )
+}
